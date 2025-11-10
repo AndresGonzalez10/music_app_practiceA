989b214135ad41810216b19b8a9e9c213a405ae2
Andres <andres.edu.glez@gmail.com>
2025-11-09 18:07:19 -0600

    nueva carpeta

---
+++ 243726MusicApp/src/app/core/components/image-area/image-area.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-area',
+  imports: [],
+  templateUrl: './image-area.html',
+  styleUrl: './image-area.css',
+})
+export class ImageArea {
+
+}
