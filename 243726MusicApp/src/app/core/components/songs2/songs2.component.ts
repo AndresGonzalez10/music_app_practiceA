bc11b1025eaf65e1cb3118751797c86360938bd9
Andres <andres.edu.glez@gmail.com>
2025-11-10 02:50:59 -0600

    otro componente de canciones

---
+++ 243726MusicApp/src/app/core/components/songs2/songs2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-songs2',
+  imports: [],
+  templateUrl: './songs2.html',
+  styleUrl: './songs2.css',
+})
+export class Songs2Component {
+
+}
--- 243726MusicApp/src/app/pages/mainview/mainview.ts
+++ 243726MusicApp/src/app/pages/mainview/mainview.ts
@@ -3,10 +3,11 @@ import { ImageAreaComponent } from '../../core/components/image-area/image-area.
 import { ImageArtisComponent } from '../../core/components/image-artis/image-artis.component';
 import { NanvBar } from "../../core/components/nanv-bar/nanv-bar";
 import { ReproductorComponent } from "../../core/components/reproductor/reproductor.component";
+import { Songs2Component } from "../../core/components/songs2/songs2.component";
 
 @Component({
   selector: 'app-mainview',
-  imports: [ImageAreaComponent, ImageArtisComponent, NanvBar, ReproductorComponent],
+  imports: [ImageAreaComponent, ImageArtisComponent, NanvBar, ReproductorComponent, Songs2Component],
   templateUrl: './mainview.html',
   styleUrl: './mainview.css',
 })
