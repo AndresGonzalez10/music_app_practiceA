da02a02bc6836a09a6a28a70fe850945dc70a1a2
Andres <andres.edu.glez@gmail.com>
2025-11-10 02:41:22 -0600

    nueva vista de reproductor

--- 243726MusicApp/src/app/pages/reproductor-view/reproductor-view.ts
+++ 243726MusicApp/src/app/pages/reproductor-view/reproductor-view.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { ProgresBarComponent } from '../../core/components/progres-bar/progres-bar.component';
+import { PlaylistComponent } from '../../core/components/playlist/playlist.component';
+import { ImageReproductorComponent } from '../../core/components/image-reproductor/image-reproductor.component';
+import { SearchComponent } from '../../core/components/search/search.component';
 
 @Component({
   selector: 'app-reproductor-view',
-  imports: [],
+  imports: [ProgresBarComponent, PlaylistComponent, ImageReproductorComponent, SearchComponent],
   templateUrl: './reproductor-view.html',
   styleUrl: './reproductor-view.css',
 })
